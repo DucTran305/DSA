ad2c791095a42d8720b11280de72787dabd1458f
DucTran305 <77599411+DucTran305@users.noreply.github.com>
2023-12-26 23:47:37 +0700

    Add files via upload

---
+++ thuchanh1.py
@@ -0,0 +1,20 @@
+# bai 1
+print('Hello World')
+
+# bai 2
+x = "hello world";
+print(x);
+
+# bai 3
+x = "hello";
+y = "world";
+z = x + " " + y;
+print(z);
+
+# bai 4
+x = "hello"
+y = "world"
+z = x + " " + y
+def hamsoin():
+    print(z)
+hamsoin()
